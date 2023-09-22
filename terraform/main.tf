ed0f6b6793c7eb71a7647e5d4c1e3f6ad59bc2af
Iamneo <techadmin@iamneo.ai>
2023-09-22 15:18:05 +0000

    Backup Commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,15 @@
-terraform {
-    required_providers {
-      aws = {
-          source = hashicorp/aws
-          version = ""
-      }
-    }
-    
+
+provider "aws" {
+    region = "ap-northeast-1"
+    access_key = "AKIAUSKQ624RNGFSV7OH"
+    secret_key = "/kiKjr2Run6PvY2H/182onerfFbV54++TpMkdnSK"
+}
+
+resource "aws-instance" "sample_instance" {
+    ami = "ami-0ed99df77a82560e6"
+    instance_type = "t2.micro"
+}
+
+output "public_ip" {
+    value = aws_instance.sample_instance.public_ip
 }
